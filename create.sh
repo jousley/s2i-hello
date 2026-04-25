d2411a931cc39ec18a4ae7267a68ee3f0e2931c0
Jeff Ousley <jousley@us.ibm.com>
2026-04-24 18:12:31 -0700

    stooopid

--- create.sh
+++ create.sh
@@ -4,6 +4,19 @@ oc new-app https://github.com/jousley/s2i-hello.git --name=nginx-hello
 
 sleep 5
 
+oc patch service nginx-hello -n jousley --type='json' -p='[
+  {
+    "op": "replace",
+    "path": "/spec/ports/0/port",
+    "value": 80
+  },
+  {
+    "op": "replace",
+    "path": "/spec/ports/0/name",
+    "value": "80-tcp"
+  }
+]'
+
 oc expose svc nginx-hello
 
 oc get route
